b60a5ff9d3605ae363ccfcb18506072e5adbbbec
Syed <syedbarkath980@gmail.com>
2026-07-21 12:51:56 +0530

    feat: Setup MongoDB connection, User model, and signup API endpoint

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,36 @@
 import express from "express"
 import connectDB from "./config/database.js"
+import User from "./models/user.js"
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.end("Hello from the SERVER!")
+app.post("/signup", async (req, res) => {
+    const userObj = {
+        firstName: "Syed",
+        lastName: "Ali",
+        email: "syedbarkath980@gmail.com",
+        password: "hello@123",
+        age: 21,
+        gender : "male"
+    }
+
+    try {
+        const user = new User(userObj)
+        await user.save()
+        res.status(200).send("Created User Successfully!")   
+    }
+    catch (err) {
+        console.log("Cant create a user!!!")
+        res.status(404).send("cant create a user")
+    }
+
 })
 
 connectDB().
     then(() => {
     console.log("Connected to DATABASE SUCCESSFULLY...")
 
-    app.listen(3000, () => {
+    app.listen(7777, () => {
         console.log("Listening on PORT 3000...")
     })
     })
---
+++ Backend/src/models/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+    firstName: {
+        type : String
+    },
+    lastName: {
+        type : String
+    },
+    email: {
+        type : String
+    },
+    password: {
+        type : String
+    },
+    age: {
+        type : Number
+    },
+    gender: {
+        type : String
+    }
+})
+
+const User = mongoose.model("Users", userSchema)
+
+export default User
